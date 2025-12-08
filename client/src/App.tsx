41758902745d75daaa40290c836b7841d5b91ec8
Arman karimi <arman.ui.dev@gmail.com>
2025-12-09 00:04:49 +0330

    refactor(client): update App routing to match hierarchical province/employee structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,49 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import LoginForm from "./pages/LoginFormPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 import GlobalAdminDashboard from "./pages/GlobalAdminDashboardPage";
-import ENDPOINTS from "./const/endpoints";
+import ProvinceEmployeesPage from "./pages/ProvinceEmployeesPage";
+import EmployeePage from "./pages/EmployeePage";
+import NewEmployeeFormPage from "./pages/NewEmployeeFormPage";
+import { ROUTES } from "./const/endpoints";
 
 function App() {
 	return (
 		<Routes>
-			<Route path={ENDPOINTS.ROOT} element={<LoginForm />}></Route>
+			<Route path={ROUTES.ROOT} element={<LoginForm />} />
 			<Route
-				path={ENDPOINTS.EMPLOYEES_GLOBAL}
+				path={ROUTES.PROVINCES}
 				element={
 					<ProtectedRoute>
 						<GlobalAdminDashboard />
 					</ProtectedRoute>
 				}
-			></Route>
+			/>
 			<Route
-				path={ENDPOINTS.EMPLOYEES_PROVINCE}
+				path={ROUTES.PROVINCE_EMPLOYEES}
 				element={
 					<ProtectedRoute>
-						< />
+						<ProvinceEmployeesPage />
 					</ProtectedRoute>
 				}
-			></Route>
-
+			/>
 			<Route
-				path={ENDPOINTS.EMPLOYEES_GET_ONE}
+				path={ROUTES.PROVINCE_EMPLOYEE_NEW}
 				element={
 					<ProtectedRoute>
-						< />
+						<NewEmployeeFormPage />
 					</ProtectedRoute>
 				}
-			></Route>
+			/>
+			<Route
+				path={ROUTES.PROVINCE_EMPLOYEE_DETAIL}
+				element={
+					<ProtectedRoute>
+						<EmployeePage />
+					</ProtectedRoute>
+				}
+			/>
+			<Route path="*" element={<Navigate to={ROUTES.ROOT} />} />
 		</Routes>
 	);
 }
