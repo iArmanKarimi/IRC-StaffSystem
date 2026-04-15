19f0c44d7729ddcd7009977c73e17a2e4b55e0f3
Arman karimi <arman.ui.dev@gmail.com>
2026-04-16 01:58:12 +0330

    chore: lazy load app routes and clean stray files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,59 +1,67 @@
+import { lazy, Suspense } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import LoginForm from "./pages/LoginFormPage";
 import ProtectedRoute from "./components/ProtectedRoute";
-import GlobalAdminDashboard from "./pages/GlobalAdminDashboardPage";
-import AdminDashboard from "./pages/AdminDashboardPage";
-import ProvinceEmployeesPage from "./pages/ProvinceEmployeesPage";
-import EmployeePage from "./pages/EmployeePage";
-import NewEmployeeFormPage from "./pages/NewEmployeeFormPage";
 import { ROUTES } from "./const/endpoints";
+// load on demand
+const LoginForm = lazy(() => import("./pages/LoginFormPage"));
+const GlobalAdminDashboard = lazy(
+	() => import("./pages/GlobalAdminDashboardPage"),
+);
+const AdminDashboard = lazy(() => import("./pages/AdminDashboardPage"));
+const ProvinceEmployeesPage = lazy(
+	() => import("./pages/ProvinceEmployeesPage"),
+);
+const EmployeePage = lazy(() => import("./pages/EmployeePage"));
+const NewEmployeeFormPage = lazy(() => import("./pages/NewEmployeeFormPage"));
 
 function App() {
 	return (
-		<Routes>
-			<Route path={ROUTES.ROOT} element={<LoginForm />} />
-			<Route
-				path={ROUTES.PROVINCES}
-				element={
-					<ProtectedRoute>
-						<GlobalAdminDashboard />
-					</ProtectedRoute>
-				}
-			/>
-			<Route
-				path={ROUTES.ADMIN_DASHBOARD}
-				element={
-					<ProtectedRoute>
-						<AdminDashboard />
-					</ProtectedRoute>
-				}
-			/>
-			<Route
-				path={ROUTES.PROVINCE_EMPLOYEES}
-				element={
-					<ProtectedRoute>
-						<ProvinceEmployeesPage />
-					</ProtectedRoute>
-				}
-			/>
-			<Route
-				path={ROUTES.PROVINCE_EMPLOYEE_NEW}
-				element={
-					<ProtectedRoute>
-						<NewEmployeeFormPage />
-					</ProtectedRoute>
-				}
-			/>
-			<Route
-				path={ROUTES.PROVINCE_EMPLOYEE_DETAIL}
-				element={
-					<ProtectedRoute>
-						<EmployeePage />
-					</ProtectedRoute>
-				}
-			/>
-			<Route path="*" element={<Navigate to={ROUTES.ROOT} />} />
-		</Routes>
+		<Suspense>
+			<Routes>
+				<Route path={ROUTES.ROOT} element={<LoginForm />} />
+				<Route
+					path={ROUTES.PROVINCES}
+					element={
+						<ProtectedRoute>
+							<GlobalAdminDashboard />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path={ROUTES.ADMIN_DASHBOARD}
+					element={
+						<ProtectedRoute>
+							<AdminDashboard />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path={ROUTES.PROVINCE_EMPLOYEES}
+					element={
+						<ProtectedRoute>
+							<ProvinceEmployeesPage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path={ROUTES.PROVINCE_EMPLOYEE_NEW}
+					element={
+						<ProtectedRoute>
+							<NewEmployeeFormPage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path={ROUTES.PROVINCE_EMPLOYEE_DETAIL}
+					element={
+						<ProtectedRoute>
+							<EmployeePage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route path="*" element={<Navigate to={ROUTES.ROOT} />} />
+			</Routes>
+		</Suspense>
 	);
 }
 
